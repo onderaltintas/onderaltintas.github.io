329d58e288da07859e7613b425f3440404e257c2
Önder <onderaltintas@gmail.com>
2019-02-12 07:36:05 +0300

    More puzzles
    to test randomization

--- guess-the-words/src/js/dictionary.js
+++ guess-the-words/src/js/dictionary.js
@@ -6,8 +6,38 @@ function Dictionary(){
     hint: "JAVA",
     hintPicture: "http://www.moc.com/a.jpg"
   },{
+    sentence: "Apple",
+    difficulty: 1,
+    category: "Vegetable",
+    hint: "Red",
+    hintPicture: "http://www.moc.com/a.jpg"
+  },{
+    sentence: "Pencil",
+    difficulty: 1,
+    category: "School",
+    hint: "Writes",
+    hintPicture: "http://www.moc.com/a.jpg"
+  },{
     sentence: "Terminator 2",
     difficulty: 2,
+    category: "Movies",
+    hint: "I'll back",
+    hintPicture: "http://www.moc.com/a.jpg"
+  },{
+    sentence: "Mortal Kombat",
+    difficulty: 2,
+    category: "Games",
+    hint: "C with K",
+    hintPicture: "http://www.moc.com/a.jpg"
+  },{
+    sentence: "I will be back",
+    difficulty: 3,
+    category: "Movie quotes",
+    hint: "I'll back",
+    hintPicture: "http://www.moc.com/a.jpg"
+  },{
+    sentence: "Astala vista baby",
+    difficulty: 3,
     category: "Movie quotes",
     hint: "I'll back",
     hintPicture: "http://www.moc.com/a.jpg"
