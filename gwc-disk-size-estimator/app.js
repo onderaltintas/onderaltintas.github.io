a0b3887ba098d156188c64854bd09eec9d13fae9
Önder <onderaltintas@gmail.com>
2024-08-08 03:24:13 +0300

    update
    last last

--- gwc-disk-size-estimator/app.js
+++ gwc-disk-size-estimator/app.js
@@ -1,5 +1,7 @@
 var diskSizesForZoomLevels = [0, 0, 0, 0, 0, 0, 0, 1, 3, 10, 40, 160, 640, 2561, 10241, 40963, 163845, 655370, 2621460, 10485800, 41943120, 167772320, 671089280, 2684356480, 10737424640, 42949693440];
 var worldArea = 510072000; //in square km
+var calculatedBBOXArea = 0;
+var calculatedArea = 0;
 
 const vectorSource = new ol.source.Vector({
     wrapX: false
@@ -9,17 +11,19 @@ const vectorLayer = new ol.layer.Vector({
     source: vectorSource,
 });
 
-var extentSource = new ol.source.Vector({wrapX: false});
+var extentSource = new ol.source.Vector({
+    wrapX: false
+});
 var extentLayer = new ol.layer.Vector({
-  source: extentSource,
-  style: new ol.style.Style({
-    fill: new ol.style.Fill({
-      color: 'rgba(0,175,0,0.2)'
-    }),
-    stroke: new ol.style.Stroke({
-      color: 'Red'
+    source: extentSource,
+    style: new ol.style.Style({
+        fill: new ol.style.Fill({
+            color: 'rgba(0,175,0,0.0)'
+        }),
+        stroke: new ol.style.Stroke({
+            color: 'Red'
+        })
     })
-  })
 })
 
 var osmLayer = new ol.layer.Tile({
@@ -31,7 +35,7 @@ const view = new ol.View({
 })
 
 const map = new ol.Map({
-    layers: [osmLayer, vectorLayer,extentLayer],
+    layers: [osmLayer, vectorLayer, extentLayer],
     target: 'olContainer',
     view: view
 });
@@ -49,7 +53,7 @@ const draw = new ol.interaction.Draw({
         image: new ol.style.Circle({
             radius: 5,
             fill: new ol.style.Fill({
-                color: [255, 0, 0, 0.8]
+                color: [255, 0, 0, 0.2]
             })
         }),
     })
@@ -61,7 +65,7 @@ draw.on('drawstart', function (event) {
 });
 draw.on('drawend', function (event) {
     var drawnGeometry = event.feature.getGeometry();
-    console.log("Drawn geom area: " + Math.round(ol.sphere.getArea(drawnGeometry)/1000000) +"km^2");
+    console.log("Drawn geom area: " + Math.round(ol.sphere.getArea(drawnGeometry) / 1000000) + "km^2");
     var extent = drawnGeometry.getExtent();
     var extentPoly = ol.geom.Polygon.fromExtent(extent);
     var feature = new ol.Feature({
@@ -69,13 +73,22 @@ draw.on('drawend', function (event) {
         geometry: extentPoly
     });
     extentSource.addFeature(feature);
-    var areaInKmSquare = Math.round(ol.sphere.getArea(extentPoly)/1000000);
-    var diskSizeForZoomLevel = diskSizesForZoomLevels[document.getElementById("maxZoom").value];
-    var combinations = document.getElementById("paramCombinations").value;
-    document.getElementById("area").innerHTML = areaInKmSquare;
-    document.getElementById("diskSize").innerHTML = Math.round(areaInKmSquare * diskSizeForZoomLevel * combinations / (worldArea * 1024));
+    calculatedBBOXArea = Math.round(ol.sphere.getArea(extentPoly) / 1000000);
+    calculatedArea = Math.round(ol.sphere.getArea(drawnGeometry) / 1000000);
+    calculate();
+});
 
-})
+function calculate() {
+    var maxZoomLevel = document.getElementById("maxZoom").value;
+    maxZoomLevel = maxZoomLevel > 25 ? 25 : maxZoomLevel;
+    maxZoomLevel = maxZoomLevel < 0 ? 0 : maxZoomLevel;
+    var diskSizeForZoomLevel = diskSizesForZoomLevels[maxZoomLevel];
+    var combinations = document.getElementById("paramCombinations").value;
+    document.getElementById("bboxArea").innerHTML = calculatedBBOXArea;
+    document.getElementById("area").innerHTML = calculatedArea;
+    document.getElementById("diskSize").innerHTML = Math.round(calculatedArea * diskSizeForZoomLevel * combinations / (worldArea * 1024));
+    document.getElementById("bboxDiskSize").innerHTML = Math.round(calculatedBBOXArea * diskSizeForZoomLevel * combinations / (worldArea * 1024));
+}
 
 var istExtent = new ol.extent.boundingExtent([[25.0, 35.0], [46, 43]]);
 istExtent = ol.proj.transformExtent(istExtent, ol.proj.get('EPSG:4326'), ol.proj.get('EPSG:3857'));
