0a68a57e6de93bbcf35f15750e94fe8f352a83dd
Önder <onderaltintas@gmail.com>
2025-02-10 23:53:37 +0300

    Update app.js

--- last-earthquakes-on-turkey-by-deepseek/with-cesium/app.js
+++ last-earthquakes-on-turkey-by-deepseek/with-cesium/app.js
@@ -1,6 +1,7 @@
 import { EarthquakeApp } from './EarthquakeApp.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+  Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI1N2NlNDZlZS01NTBkLTRkMDktOWMxZi04M2U1MTc0NDE3NDciLCJpZCI6MTU4MjUsImlhdCI6MTczOTIyMDQ2OX0.-TK7EMlZfJNg654qL2wfJv2jbBrmIYIEYe5lIcBsRh4';
   const app = new EarthquakeApp();
   app.init();
 });
