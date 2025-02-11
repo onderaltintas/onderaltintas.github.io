7418fb70b8ee1f8ecbf6bc690ae2347b9725168e
Önder <onderaltintas@gmail.com>
2025-02-11 03:06:33 +0300

    Update app.js
    async batyhmetry to app as param

--- last-earthquakes-on-turkey-by-deepseek/with-cesium/app.js
+++ last-earthquakes-on-turkey-by-deepseek/with-cesium/app.js
@@ -2,6 +2,7 @@ import { EarthquakeApp } from './EarthquakeApp.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI1N2NlNDZlZS01NTBkLTRkMDktOWMxZi04M2U1MTc0NDE3NDciLCJpZCI6MTU4MjUsImlhdCI6MTczOTIyMDQ2OX0.-TK7EMlZfJNg654qL2wfJv2jbBrmIYIEYe5lIcBsRh4';
-  const app = new EarthquakeApp();
+  let bathymetry = await Cesium.createWorldBathymetryAsync({requestVertexNormals: true});
+  const app = new EarthquakeApp(bathymetry);
   app.init();
 });
