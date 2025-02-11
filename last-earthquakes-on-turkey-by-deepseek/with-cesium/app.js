c6aa642b80b126f5b408a33a2e50e706a72c7857
Önder <onderaltintas@gmail.com>
2025-02-11 03:11:16 +0300

    Update app.js
    qwd

--- last-earthquakes-on-turkey-by-deepseek/with-cesium/app.js
+++ last-earthquakes-on-turkey-by-deepseek/with-cesium/app.js
@@ -2,7 +2,13 @@ import { EarthquakeApp } from './EarthquakeApp.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI1N2NlNDZlZS01NTBkLTRkMDktOWMxZi04M2U1MTc0NDE3NDciLCJpZCI6MTU4MjUsImlhdCI6MTczOTIyMDQ2OX0.-TK7EMlZfJNg654qL2wfJv2jbBrmIYIEYe5lIcBsRh4';
-  let bathymetry = await Cesium.createWorldBathymetryAsync({requestVertexNormals: true});
-  const app = new EarthquakeApp(bathymetry);
-  app.init();
+  Cesium.createWorldBathymetryAsync({requestVertexNormals: true}).then(
+    function(provider) {
+      const app = new EarthquakeApp(provider);
+      app.init();
+    },
+    function(error) {
+      console.error(error);
+    }
+  );
 });
